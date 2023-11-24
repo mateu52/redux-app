30cbf4d7dcb278185ff01cd1e45dd17dab9b0677
mateu52 <wuoelte@gmail.com>
2023-11-24 18:25:47 +0100

    message action  in form component

--- src/components/Message/reducer.js
+++ src/components/Message/reducer.js
@@ -1,7 +1,9 @@
 const INFO_MSG = "INFO_MSG";
 const WARNING_MSG = "WARNING_MSG";
 const DANGER_MSG = "DANGER_MSG";
-const ZERO_MSG = "ZERO_MSG"
+const ZERO_MSG = "ZERO_MSG";
+const SEND_ED = "SEND_ED";
+const FAIL_SENDING = "FAIL_SENDING";
 
 const INITIAL_STATE = {
     info: '',
@@ -13,6 +15,8 @@ export const info = () => ({ type: INFO_MSG, payload: 'pobrano z API' })
 export const warning = () => ({ type: WARNING_MSG, payload: 'uwaga' })
 export const danger = () => ({ type: DANGER_MSG, payload: 'uwaga, zresetowano liste' })
 export const to_zero = () => ({ type: ZERO_MSG })
+export const sended = () => ({ type: SEND_ED, payload: 'wysłano dane'})
+export const fail_send = () => ({ type: FAIL_SENDING, payload: 'nie udało się wysłać danych'})
 
 
 export default (state = INITIAL_STATE, action) => {
@@ -44,6 +48,20 @@ export default (state = INITIAL_STATE, action) => {
                 info: '',
                 warning: '',
                 danger: ''
+            }
+        case SEND_ED:
+            return {
+                ...state,
+                info: action.payload,
+                warning: '',
+                danger: ''
+            }
+        case FAIL_SENDING:
+            return {
+                ...state,
+                info: '',
+                warning: '',
+                danger: action.payload
             }
         default:
             return state
--- src/components/Pages/Form/Form.js
+++ src/components/Pages/Form/Form.js
@@ -1,26 +1,52 @@
-//impot React from 'react';
+//import React from 'react';
 import { useForm } from 'react-hook-form'
-export default function App() {
+import { connect, useDispatch } from 'react-redux';
+import { sended, fail_send } from '../../Message/reducer';
+import Message from '../../Message/Containers/Message';
+function Form({ dis_send, dis_fail }) {
     const {
         register,
         handleSubmit,
-        watch,
+        setError,
+        clearErrors,
         formState: { errors},
     } = useForm();
 
-    const onSubmit = (data) => console.log(data)
-    console.log(watch("example"))
+    const onSubmit = (data) => {
+        console.log('Wartość data.exampleRequired:', data.exampleRequired);
+        console.log('Negacja data.exampleRequired:', !data.exampleRequired);
+        clearErrors('exampleRequired');
+        if (!data.exampleRequired) {
+            // Ustawienie błędu, jeśli warunek nie jest spełniony
+            setError('exampleRequired', {
+                type: 'manual',
+                message: 'This field is required',
+                
+            });
+            dis_fail();
+        } else {
+            dis_send();
+        }
+    }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col'>
-            <input defaultValue="test" {...register("example")} className='text-center m-auto bg-gray-300' />
+            <input defaultValue="test" {...register("example")} className='text-center m-auto bg-green-100 mt-4' />
 
-            <input {...register("exampleRequired", {required: true})} className='text-center m-auto'/>
-            {errors.exampleRequired && <span>This field is required</span>}
+            <input {...register("exampleRequired")} className='text-center m-auto bg-green-100 mt-2 mb-2'/>
+            {errors.exampleRequired && <span>{errors.exampleRequired.message}
+                {/* {dis_fail()} */}
+            </span>}
 
-            <input type="submit" />
+            <input type="submit" className='m-auto p-1 bg-green-400'/>
+            <Message />
         </form>
+        
     )
-
-
 }
+
+const mapDispatchToProps = dispatch => ({
+    dis_send: () => dispatch(sended()),
+    dis_fail: () => dispatch(fail_send())
+})
+export default connect(null,mapDispatchToProps)(Form)
